3c48e3b586d0f61bb4a03c5ca9a88e8d56b08784
Mateus Sandes <mateussandes@hotmail.com.br>
2026-04-14 17:14:52 -0300

    Configurado o consumer para lindar com falhas no consumo das mensagens do topico

--- demo-consumer/src/main/java/br/com/sandes/config/KafkaConsumerConfig.java
+++ demo-consumer/src/main/java/br/com/sandes/config/KafkaConsumerConfig.java
@@ -1,10 +1,10 @@
 package br.com.sandes.config;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import br.com.sandes.events.GetClientCreatedEvent;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,13 +12,25 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
+import org.springframework.util.backoff.ExponentialBackOff;
 
-import br.com.sandes.events.GetClientCreatedEvent;
+import java.util.HashMap;
+import java.util.Map;
 
 @EnableKafka
 @Configuration
 public class KafkaConsumerConfig {
+
+	private static final Logger log = LoggerFactory.getLogger(KafkaConsumerConfig.class);
+
+	@Value(value = "${spring.kafka.backoff.interval}")
+	private Long interval;
+
+	@Value(value = "${spring.kafka.backoff.max_failure}")
+	private Long maxAttempts;
 	
 	@Value(value = "${spring.kafka.bootstrap-servers}")
 	private String bootstrapAddress;
@@ -46,16 +58,39 @@ public class KafkaConsumerConfig {
 				new StringDeserializer(),
 				new JacksonJsonDeserializer<>(GetClientCreatedEvent.class));
 	}
+
+	@Bean
+	DefaultErrorHandler errorHandler () {
+		ExponentialBackOff fixedBackOff = new ExponentialBackOff(interval, maxAttempts);
+
+        DefaultErrorHandler defaultErrorHandler = new DefaultErrorHandler (
+				(consumerRecord, exception) -> {
+					log.error("Erro durante o consumo da mensagem do topico {}: {}",
+							consumerRecord.topic(), exception.getMessage()
+					);
+				}, fixedBackOff
+		);
+
+		defaultErrorHandler.addNotRetryableExceptions(
+				IllegalArgumentException.class
+		);
+
+		return defaultErrorHandler;
+	}
 	
 	@Bean
 	ConcurrentKafkaListenerContainerFactory<String, GetClientCreatedEvent>
 		kafkaListenerContainerFactory () {
-		
+
 		ConcurrentKafkaListenerContainerFactory<String, GetClientCreatedEvent> factory =
 				new ConcurrentKafkaListenerContainerFactory<>();
-		
+
 		factory.setConsumerFactory(consumerFactory());
-		
+
+		factory.setConcurrency(3);
+		factory.setCommonErrorHandler(errorHandler());
+		factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.RECORD);
+
 		return factory;
- 	}
+	}
 }
